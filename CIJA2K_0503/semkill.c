5051e5741ad89d79ca69a9e46ae4080c5c1b674c
Zenekes <78820468+Zenekes@users.noreply.github.com>
2021-05-12 21:53:06 +0200

    Add files via upload

---
+++ CIJA2K_0503/semkill.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#define SEMKEY 123456L
+
+    int semid,nsems,rtn;
+    int semflg;
+    struct sembuf sembuf, *sop;
+    union semun{int val;
+    struct semid_ds *buf;
+	unsigned short *array;}arg;
+    int cmd;
+
+main()
+{
+    nsems = 1;
+    semflg = 00666 | IPC_CREAT;
+    semid = semget (SEMKEY, nsems, semflg);
+    if (semid < 0 ) {perror(" semget hiba"); exit(0);}
+    else printf("\n semid: %d ",semid);
+    printf ("\n");
+
+    cmd = IPC_RMID;
+    rtn = semctl(semid,0, cmd, arg);
+    printf("\n Kill rtn: %d ",rtn);
+    printf("\n");
+
+}
